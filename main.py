dae5cacb5ec72cdf70fef22439f43731399c397e
Julya <86087548+Ju-Vilela@users.noreply.github.com>
2025-02-24 18:35:35 -0300

    Create main.py
    From Replit

---
+++ main.py
@@ -0,0 +1,32 @@
+print('Hi... World?\n');
+
+# 'sep' string inserida entre os valores.
+print('DD', 'MM', 'YYYY', sep="/");
+
+# 'end' string anexada após o ultimo valor.
+print("Idade: ", end="");
+
+# ! Não precisa definir o tipo da variavel.
+# A variavel só passa a existir quando atribui um valor a ela
+variavel = "00 anos";
+print(variavel);
+
+# Tipos de valores
+print('', type(0), type(1.3), type("x"), type(True), sep="\n");
+
+# Teste: Só pode escrever multiplas linhas com aspas triplicadas('/")
+print("""
+  How are you?
+""")
+
+# Implementar variável na string do print.
+a = 11
+b = 23
+
+print("Dia {} do Mes {}".format(b, a))
+
+
+# ! 'Pycharm' IDE focada exclusivamente em pyhton.
+# Permitindo a criação de uma aplicação mais facilmente.
+
+# Iniciando no Python com o Alura
